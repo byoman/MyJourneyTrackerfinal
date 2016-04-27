ac8ee53df1bf58f59899196a8d372a92320a4751
byoman <yann.bonnet@epita.fr>
2016-04-27 12:25:20 +0200

    Fourth commit
    custom view initialized to be square sized, with white lines

--- MyJourneyTrackerFinal/app/src/main/java/com/example/dragonne/myjourneytrackerfinal/Graph.java
+++ MyJourneyTrackerFinal/app/src/main/java/com/example/dragonne/myjourneytrackerfinal/Graph.java
@@ -2,6 +2,7 @@ package com.example.dragonne.myjourneytrackerfinal;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.view.View;
  * Created by Dragonne on 27/04/2016.
  */
 public class Graph extends View{
+    private int width,height;
     public Graph(Context context,AttributeSet attrs){
         super(context,attrs);
     }
@@ -17,5 +19,25 @@ public class Graph extends View{
     protected void onDraw(Canvas canvas){
         super.onDraw(canvas);
         Paint paint = new Paint();
+        paint.setColor(Color.BLACK);
+        canvas.drawRect(0,0,width,height,paint);
+        paint.setColor(Color.WHITE);
+        canvas.drawLine(0,height/6,width,height/6,paint);//1st line
+        canvas.drawLine(0,2*height/6,width,2*height/6,paint);//2nd line
+        canvas.drawLine(0,3*height/6,width,3*height/6,paint); //3rd line
+        canvas.drawLine(0,4*height/6,width,4*height/6,paint); //4th line
+        canvas.drawLine(0,5*height/6,width,5*height/6,paint); //5th line
     }
+    @Override
+    protected void onSizeChanged(int xNew,int yNew,int xOld,int yOld){
+        super.onSizeChanged(xNew,yNew,xOld,yOld);
+        if(xNew<yNew){
+            width = xNew;
+            height = xNew;
+        } else {
+            width = yNew;
+            height = yNew;
+        }
+    }
+
 }
